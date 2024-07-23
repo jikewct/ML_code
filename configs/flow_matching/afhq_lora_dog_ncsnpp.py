e0686466b7426c2db0722e8bab5869924ab5f7c9
jikewct <jikewct@qq.com>
2024-07-24 00:07:14 +0800

    add lora implementation

---
+++ configs/flow_matching/afhq_lora_dog_ncsnpp.py
@@ -0,0 +1,110 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Train the original DDPM model."""
+
+from configs.default_afhq_configs import get_default_configs
+
+
+def get_config():
+    config = get_default_configs()
+    training = config.training
+    model = config.model
+    sampling = config.sampling
+    data = config.data
+    fast_fid = config.fast_fid
+    optim = config.optim
+    test = config.test
+
+    training.model_checkpoint = "./data/checkpoints/generative_model/flowMatching/lora_ncsnpp-afhq-24000-model"
+    # training.model_checkpoint = "/home/jikewct/public/jikewct/Model/rect_flow/CelebA-HQ-Pytorch_model_ema.pth"
+    training.continuous = False
+    training.batch_size = 32
+    training.epochs = 1000
+    training.snapshot_freq = 2000
+    training.log_freq = 100
+    training.eval_freq = 1000
+    training.test_metric_freq = 100000
+
+    model.name = "flowMatching"
+    model.nn_name = "lora_ncsnpp"
+
+    # model.norm = "gn"
+    # model.activation = "elu"
+
+    model.scale_by_sigma = False
+    model.ema_rate = 0.999
+    model.dropout = 0.0
+    model.norm = "GroupNorm"
+    model.activation = "swish"
+    model.nf = 128
+    model.channel_mults = (
+        1,
+        1,
+        2,
+        2,
+    )
+    model.num_res_blocks = 2
+    model.attention_resolutions = (8,)
+    model.resamp_with_conv = True
+    model.conditional = True
+    model.fir = True
+    model.fir_kernel = [1, 3, 3, 1]
+    model.skip_rescale = True
+    model.resblock_type = "biggan"
+    model.progressive = "output_skip"
+    model.progressive_input = "input_skip"
+    model.progressive_combine = "sum"
+    model.attention_type = "ddpm"
+    model.init_scale = 0.0
+    model.embedding_type = "fourier"
+    model.fourier_scale = 16
+    model.conv_size = 3
+
+    model.sigma_dist = "geometric"
+    model.sigma_max = 50
+    model.sigma_min = 0.01
+    model.num_scales = 1000
+
+    model.lora_dim = 32
+    model.lora_alpha = 32
+    model.lora_dropout = 0.0
+    model.enable_lora = [True]
+
+    data.category = "church_outdoor_train"
+    data.img_size = (64, 64)
+    data.img_class = "dog"
+
+    test.save_path = "./data/test/"
+    test.batch_size = 10
+    test.num_samples = 2
+
+    fast_fid.num_samples = 10000
+    fast_fid.begin_step = 5000
+    fast_fid.end_step = 1000000
+    fast_fid.batch_size = 32
+
+    sampling.sample_steps = 10
+    sampling.denoise = True
+    sampling.log_freq = 1
+    sampling.rtol = 1e-3
+    sampling.atol = 1e-3
+    sampling.method = "rk45"
+
+    optim.lr = 1e-4
+    config.pipeline = "FlowMatchingPipeLine"
+
+    return config
---
+++ pipeline/models/network/lora_ncsnpp.py
@@ -0,0 +1,476 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: skip-file
+
+import functools
+import logging
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from . import layers, layerspp, net_utils, normalization, lora_layers
+
+ResnetBlockDDPM = layerspp.ResnetBlockDDPMpp
+ResnetBlockBigGAN = layerspp.ResnetBlockBigGANpp
+LoraResnetBlockBigGAN = layerspp.LoRAResnetBlockBigGANpp
+
+Combine = layerspp.Combine
+conv3x3 = layers.ddpm_conv3x3
+lora_conv3x3 = layers.lora_ddpm_conv3x3
+conv1x1 = layerspp.conv1x1
+get_act = layers.get_act
+# get_normalization = normalization.get_normalization
+default_initializer = layers.default_init
+
+
+@net_utils.register_network(name="lora_ncsnpp")
+class NCSNpp(nn.Module):
+    """NCSN++ model"""
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.act = act = get_act(config.model.activation)
+        self.register_buffer("sigmas", net_utils.get_sigmas(config))
+
+        self.nf = nf = config.model.nf
+        ch_mult = config.model.channel_mults
+        self.num_res_blocks = num_res_blocks = config.model.num_res_blocks
+        self.attn_resolutions = attn_resolutions = config.model.attention_resolutions
+        dropout = config.model.dropout
+        resamp_with_conv = config.model.resamp_with_conv
+        self.num_resolutions = num_resolutions = len(ch_mult)
+        self.all_resolutions = all_resolutions = [config.data.img_size[0] // (2**i) for i in range(num_resolutions)]
+
+        self.conditional = conditional = config.model.conditional  # noise-conditional
+        fir = config.model.fir
+        fir_kernel = config.model.fir_kernel
+        self.skip_rescale = skip_rescale = config.model.skip_rescale
+        self.resblock_type = resblock_type = config.model.resblock_type.lower()
+        self.progressive = progressive = config.model.progressive.lower()
+        self.progressive_input = progressive_input = config.model.progressive_input.lower()
+        self.embedding_type = embedding_type = config.model.embedding_type.lower()
+        init_scale = config.model.init_scale
+        self.lora_dim = config.model.lora_dim
+        self.lora_alpha = config.model.lora_alpha
+        self.lora_dropout = config.model.lora_dropout
+        self.enable_lora = config.model.enable_lora
+        assert progressive in ["none", "output_skip", "residual"]
+        assert progressive_input in ["none", "input_skip", "residual"]
+        assert embedding_type in ["fourier", "positional"]
+        combine_method = config.model.progressive_combine.lower()
+        combiner = functools.partial(Combine, method=combine_method)
+
+        modules = []
+        # timestep/noise_level embedding; only for continuous training
+        if embedding_type == "fourier":
+            # Gaussian Fourier features embeddings.
+            # assert config.training.continuous, "Fourier features are only used for continuous training."
+
+            modules.append(layerspp.GaussianFourierProjection(embedding_size=nf, scale=config.model.fourier_scale))
+            embed_dim = 2 * nf
+
+        elif embedding_type == "positional":
+            embed_dim = nf
+
+        else:
+            raise ValueError(f"embedding type {embedding_type} unknown.")
+
+        if conditional:
+            modules.append(
+                lora_layers.LoRAMergedLinear(
+                    embed_dim,
+                    nf * 4,
+                    self.lora_dim,
+                    self.lora_alpha,
+                    self.lora_dropout,
+                    self.enable_lora,
+                    fan_in_fan_out=False,
+                    merge_weights=False,
+                )
+            )
+            modules[-1].weight.data = default_initializer()(modules[-1].weight.shape)
+            nn.init.zeros_(modules[-1].bias)
+            modules.append(
+                lora_layers.LoRAMergedLinear(
+                    nf * 4,
+                    nf * 4,
+                    self.lora_dim,
+                    self.lora_alpha,
+                    self.lora_dropout,
+                    self.enable_lora,
+                    fan_in_fan_out=False,
+                    merge_weights=False,
+                )
+            )
+            modules[-1].weight.data = default_initializer()(modules[-1].weight.shape)
+            nn.init.zeros_(modules[-1].bias)
+
+        AttnBlock = functools.partial(layerspp.AttnBlockpp, init_scale=init_scale, skip_rescale=skip_rescale)
+
+        Upsample = functools.partial(
+            layerspp.Upsample,
+            with_conv=resamp_with_conv,
+            fir=fir,
+            fir_kernel=fir_kernel,
+        )
+
+        if progressive == "output_skip":
+            self.pyramid_upsample = layerspp.Upsample(fir=fir, fir_kernel=fir_kernel, with_conv=False)
+        elif progressive == "residual":
+            pyramid_upsample = functools.partial(layerspp.Upsample, fir=fir, fir_kernel=fir_kernel, with_conv=True)
+
+        Downsample = functools.partial(
+            layerspp.Downsample,
+            with_conv=resamp_with_conv,
+            fir=fir,
+            fir_kernel=fir_kernel,
+        )
+
+        if progressive_input == "input_skip":
+            self.pyramid_downsample = layerspp.Downsample(fir=fir, fir_kernel=fir_kernel, with_conv=False)
+        elif progressive_input == "residual":
+            pyramid_downsample = functools.partial(layerspp.Downsample, fir=fir, fir_kernel=fir_kernel, with_conv=True)
+
+        if resblock_type == "ddpm":
+            ResnetBlock = functools.partial(
+                ResnetBlockDDPM,
+                act=act,
+                dropout=dropout,
+                init_scale=init_scale,
+                skip_rescale=skip_rescale,
+                temb_dim=nf * 4,
+            )
+
+        elif resblock_type == "biggan":
+            ResnetBlock = functools.partial(
+                ResnetBlockBigGAN,
+                act=act,
+                dropout=dropout,
+                fir=fir,
+                fir_kernel=fir_kernel,
+                init_scale=init_scale,
+                skip_rescale=skip_rescale,
+                temb_dim=nf * 4,
+            )
+
+        else:
+            raise ValueError(f"resblock type {resblock_type} unrecognized.")
+        LoraResnetBlock = functools.partial(
+            LoraResnetBlockBigGAN,
+            act=act,
+            dropout=dropout,
+            fir=fir,
+            fir_kernel=fir_kernel,
+            init_scale=init_scale,
+            skip_rescale=skip_rescale,
+            temb_dim=nf * 4,
+            r=self.lora_dim,
+            lora_alpha=self.lora_alpha,
+            merge_weights=True,
+        )
+        # Downsampling block
+
+        channels = config.data.img_channels
+        if progressive_input != "none":
+            input_pyramid_ch = channels
+
+        modules.append(conv3x3(channels, nf))
+        hs_c = [nf]
+
+        in_ch = nf
+        for i_level in range(num_resolutions):
+            # Residual blocks for this resolution
+            for i_block in range(num_res_blocks):
+                out_ch = nf * ch_mult[i_level]
+                modules.append(ResnetBlock(in_ch=in_ch, out_ch=out_ch))
+                in_ch = out_ch
+
+                if all_resolutions[i_level] in attn_resolutions:
+                    modules.append(AttnBlock(channels=in_ch))
+                hs_c.append(in_ch)
+
+            if i_level != num_resolutions - 1:
+                if resblock_type == "ddpm":
+                    modules.append(Downsample(in_ch=in_ch))
+                else:
+                    modules.append(ResnetBlock(down=True, in_ch=in_ch))
+
+                if progressive_input == "input_skip":
+                    modules.append(combiner(dim1=input_pyramid_ch, dim2=in_ch))
+                    if combine_method == "cat":
+                        in_ch *= 2
+
+                elif progressive_input == "residual":
+                    modules.append(pyramid_downsample(in_ch=input_pyramid_ch, out_ch=in_ch))
+                    input_pyramid_ch = in_ch
+
+                hs_c.append(in_ch)
+
+        in_ch = hs_c[-1]
+        modules.append(ResnetBlock(in_ch=in_ch))
+        modules.append(AttnBlock(channels=in_ch))
+        modules.append(LoraResnetBlock(in_ch=in_ch))
+
+        pyramid_ch = 0
+        # Upsampling block
+        for i_level in reversed(range(num_resolutions)):
+            for i_block in range(num_res_blocks + 1):
+                out_ch = nf * ch_mult[i_level]
+                if i_block % 2 == 0:
+                    modules.append(ResnetBlock(in_ch=in_ch + hs_c.pop(), out_ch=out_ch))
+                else:
+                    modules.append(LoraResnetBlock(in_ch=in_ch + hs_c.pop(), out_ch=out_ch))
+
+                in_ch = out_ch
+
+            if all_resolutions[i_level] in attn_resolutions:
+                modules.append(AttnBlock(channels=in_ch))
+
+            if progressive != "none":
+                if i_level == num_resolutions - 1:
+                    if progressive == "output_skip":
+                        modules.append(
+                            nn.GroupNorm(
+                                num_groups=min(in_ch // 4, 32),
+                                num_channels=in_ch,
+                                eps=1e-6,
+                            )
+                        )
+                        modules.append(conv3x3(in_ch, channels, init_scale=init_scale))
+                        pyramid_ch = channels
+                    elif progressive == "residual":
+                        modules.append(
+                            nn.GroupNorm(
+                                num_groups=min(in_ch // 4, 32),
+                                num_channels=in_ch,
+                                eps=1e-6,
+                            )
+                        )
+                        modules.append(conv3x3(in_ch, in_ch, bias=True))
+                        pyramid_ch = in_ch
+                    else:
+                        raise ValueError(f"{progressive} is not a valid name.")
+                else:
+                    if progressive == "output_skip":
+                        modules.append(
+                            nn.GroupNorm(
+                                num_groups=min(in_ch // 4, 32),
+                                num_channels=in_ch,
+                                eps=1e-6,
+                            )
+                        )
+                        modules.append(
+                            lora_conv3x3(
+                                in_ch,
+                                channels,
+                                bias=True,
+                                init_scale=init_scale,
+                                r=self.lora_dim,
+                                lora_alpha=self.lora_alpha,
+                                lora_dropout=self.lora_dropout,
+                                merge_weights=True,
+                            )
+                        )
+                        pyramid_ch = channels
+                    elif progressive == "residual":
+                        modules.append(pyramid_upsample(in_ch=pyramid_ch, out_ch=in_ch))
+                        pyramid_ch = in_ch
+                    else:
+                        raise ValueError(f"{progressive} is not a valid name")
+
+            if i_level != 0:
+                if resblock_type == "ddpm":
+                    modules.append(Upsample(in_ch=in_ch))
+                else:
+                    modules.append(ResnetBlock(in_ch=in_ch, up=True))
+
+        assert not hs_c
+
+        if progressive != "output_skip":
+            modules.append(nn.GroupNorm(num_groups=min(in_ch // 4, 32), num_channels=in_ch, eps=1e-6))
+            modules.append(conv3x3(in_ch, channels, init_scale=init_scale))
+
+        self.all_modules = nn.ModuleList(modules)
+
+    def forward(self, x, time_cond, y=None):
+        # timestep/noise_level embedding; only for continuous training
+        # logging.info(f"x_max:{x.max()},x_min:{x.min()}, x_mean:{x.mean()}")
+        modules = self.all_modules
+        m_idx = 0
+        if self.embedding_type == "fourier":
+            # Gaussian Fourier features embeddings.
+            used_sigmas = time_cond
+            # logging.info(f"used_sigmas:{used_sigmas.mean()}")
+            temb = modules[m_idx](torch.log(used_sigmas))
+            m_idx += 1
+            # logging.info(f"temb_norm:{torch.norm(temb)}, temb_shape:{temb.shape}")
+
+        elif self.embedding_type == "positional":
+            # Sinusoidal positional embeddings.
+            timesteps = time_cond
+            used_sigmas = self.sigmas[time_cond.long()]
+            temb = layers.get_timestep_embedding(timesteps, self.nf)
+
+        else:
+            raise ValueError(f"embedding type {self.embedding_type} unknown.")
+
+        if self.conditional:
+            temb = modules[m_idx](temb)
+            m_idx += 1
+            temb = modules[m_idx](self.act(temb))
+            m_idx += 1
+        else:
+            temb = None
+
+        if not self.config.data.centered:
+            # If input data is in [0, 1]
+            x = 2 * x - 1.0
+
+        # Downsampling block
+        input_pyramid = None
+        if self.progressive_input != "none":
+            input_pyramid = x
+
+        hs = [modules[m_idx](x)]
+        m_idx += 1
+        for i_level in range(self.num_resolutions):
+            # Residual blocks for this resolution
+            for i_block in range(self.num_res_blocks):
+                h = modules[m_idx](hs[-1], temb)
+                m_idx += 1
+                if h.shape[-1] in self.attn_resolutions:
+                    h = modules[m_idx](h)
+                    m_idx += 1
+
+                hs.append(h)
+
+            if i_level != self.num_resolutions - 1:
+                if self.resblock_type == "ddpm":
+                    h = modules[m_idx](hs[-1])
+                    m_idx += 1
+                else:
+                    h = modules[m_idx](hs[-1], temb)
+                    m_idx += 1
+
+                if self.progressive_input == "input_skip":
+                    input_pyramid = self.pyramid_downsample(input_pyramid)
+                    h = modules[m_idx](input_pyramid, h)
+                    m_idx += 1
+
+                elif self.progressive_input == "residual":
+                    input_pyramid = modules[m_idx](input_pyramid)
+                    m_idx += 1
+                    if self.skip_rescale:
+                        input_pyramid = (input_pyramid + h) / np.sqrt(2.0)
+                    else:
+                        input_pyramid = input_pyramid + h
+                    h = input_pyramid
+
+                hs.append(h)
+
+        h = hs[-1]
+        h = modules[m_idx](h, temb)
+        m_idx += 1
+        h = modules[m_idx](h)
+        m_idx += 1
+        h = modules[m_idx](h, temb)
+        m_idx += 1
+        # logging.info(f"h_norm:{torch.norm(h)}, h_shape:{h.shape}")
+        pyramid = None
+
+        # Upsampling block
+        for i_level in reversed(range(self.num_resolutions)):
+            for i_block in range(self.num_res_blocks + 1):
+                h = modules[m_idx](torch.cat([h, hs.pop()], dim=1), temb)
+                m_idx += 1
+
+            if h.shape[-1] in self.attn_resolutions:
+                h = modules[m_idx](h)
+                m_idx += 1
+            # logging.info(f"h_norm:{torch.norm(h)}, h_shape:{h.shape}")
+
+            if self.progressive != "none":
+                if i_level == self.num_resolutions - 1:
+                    if self.progressive == "output_skip":
+                        pyramid = self.act(modules[m_idx](h))
+                        m_idx += 1
+                        pyramid = modules[m_idx](pyramid)
+                        m_idx += 1
+                        # logging.info(f"pyramid_norm:{torch.norm(pyramid)}, pyramid_shape:{pyramid.shape}")
+
+                    elif self.progressive == "residual":
+                        pyramid = self.act(modules[m_idx](h))
+                        m_idx += 1
+                        pyramid = modules[m_idx](pyramid)
+                        m_idx += 1
+                    else:
+                        raise ValueError(f"{self.progressive} is not a valid name.")
+                else:
+                    if self.progressive == "output_skip":
+                        pyramid = self.pyramid_upsample(pyramid)
+                        # logging.info(f"pyramid_norm:{torch.norm(pyramid)}, pyramid_shape:{pyramid.shape}")
+
+                        pyramid_h = self.act(modules[m_idx](h))
+                        # logging.info(f"pyramid_norm:{torch.norm(pyramid)}, pyramid_shape:{pyramid.shape}")
+
+                        m_idx += 1
+                        pyramid_h = modules[m_idx](pyramid_h)
+                        # logging.info(f"pyramid_norm:{torch.norm(pyramid)}, pyramid_shape:{pyramid.shape}")
+
+                        m_idx += 1
+                        pyramid = pyramid + pyramid_h
+                        # logging.info(f"pyramid_norm:{torch.norm(pyramid)}, pyramid_shape:{pyramid.shape}")
+
+                    elif self.progressive == "residual":
+                        pyramid = modules[m_idx](pyramid)
+                        m_idx += 1
+                        if self.skip_rescale:
+                            pyramid = (pyramid + h) / np.sqrt(2.0)
+                        else:
+                            pyramid = pyramid + h
+                        h = pyramid
+                    else:
+                        raise ValueError(f"{self.progressive} is not a valid name")
+
+            if i_level != 0:
+                if self.resblock_type == "ddpm":
+                    h = modules[m_idx](h)
+                    m_idx += 1
+                else:
+                    h = modules[m_idx](h, temb)
+                    m_idx += 1
+
+        assert not hs
+        # logging.info(f"h_norm:{torch.norm(h)}, h_shape:{h.shape}")
+
+        if self.progressive == "output_skip":
+            h = pyramid
+            # logging.info(f"h_norm:{torch.norm(h)}, h_shape:{h.shape}")
+
+        else:
+            h = self.act(modules[m_idx](h))
+            m_idx += 1
+            h = modules[m_idx](h)
+            m_idx += 1
+
+        assert m_idx == len(modules)
+        if self.config.model.scale_by_sigma:
+            used_sigmas = used_sigmas.reshape((x.shape[0], *([1] * len(x.shape[1:]))))
+            h = h / used_sigmas
+            # logging.info(f"h_norm:{torch.norm(h[0])}")
+        return h, {}
